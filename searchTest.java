bbf60abd143b7067806c3d4dc8c5526606c380f4
ericsun1234567 <60624120+ericsun1234567@users.noreply.github.com>
2021-10-28 14:41:03 -0500

    Test 2

--- searchTest.java
+++ searchTest.java
@@ -21,4 +21,17 @@ class searchTest {
 		assertEquals(0,c1);
 	}
 	
+@Test
+void TestA2B1C1() {
+	FactList a = new FactList();
+	int mode = 0;
+	String query = "";
+	FactList result = a.search(query, mode);
+	int a1 = 1;
+	int b1 = mode;
+	int c1 = query.length();
+	assertEquals(1,a1);
+	assertEquals(0,b1);
+	assertEquals(0,c1);
+}
 }
